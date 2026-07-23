f36e64347ca5bcd57fcccd1ae36ee8a17ced91ad
nyxion <287495214+nyxion-luna@users.noreply.github.com>
2026-07-23 08:59:36 +0200

    release 1.0.1

--- src/rsp/__init__.py
+++ src/rsp/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __description__ = "you know those '1 hour of random sfx' videos? now with modular design, load your own sound for infinite random sounds! get yours today!"
