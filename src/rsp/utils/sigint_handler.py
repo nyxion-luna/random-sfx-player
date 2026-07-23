d7635326dab8e4c878459ed0997271a3939536a4
nyxion <287495214+nyxion-luna@users.noreply.github.com>
2026-07-23 09:03:02 +0200

    release 1.0.2, stop async audio playing on ctrl c

--- src/rsp/utils/sigint_handler.py
+++ src/rsp/utils/sigint_handler.py
@@ -1,9 +1,11 @@
+from rsp.utils.killprocs import clearPlays
 import signal
 import sys
 
 
 def setup():
     def onExit(code, stack):
+        clearPlays()
         print('\033[31;1mend: \033[33mexiting\033[0m')
         sys.exit(0)
 
